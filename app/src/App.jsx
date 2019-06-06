861555215badbfe2a9668563d42bac0b9d911046
miguel <maq1993@gmail.com>
2019-06-06 17:39:30 +0200

    Adjusts

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -90,6 +90,7 @@ class App extends Component {
       build: "gradle",
       features: [],
       featureSearch: "",
+      featuresToSelect: FEATURES,
       featureSearchResults: Object.entries({}),
       featuresSelected: Object.entries({}),
       downloading: false,
@@ -103,7 +104,7 @@ class App extends Component {
     this.setState({ featureSearch: value });
 
     if (value) {
-      const featuresMap = new Map(FEATURES);
+      const featuresMap = new Map(this.state.featuresToSelect);
 
       const keysSearched = Array.from(featuresMap.keys()).filter(
         el => el.toLowerCase().indexOf(value.toLowerCase()) > -1
@@ -133,17 +134,42 @@ class App extends Component {
 
   addFeature = feature => {
     let featuresSelected = this.state.featuresSelected;
+    let featuresToSelect = this.state.featuresToSelect;
     let features = this.state.features;
 
     featuresSelected.push(feature);
     features.push(feature[0]);
 
+    let indexOfFeature;
+
+    // eslint-disable-next-line
+    featuresToSelect.map((f, i) => {
+      if (f[0] === feature[0]) {
+        return (indexOfFeature = i);
+      }
+    });
+
+    featuresToSelect.splice(indexOfFeature, 1);
+
     this.setState({
+      featuresToSelect,
       featuresSelected,
       features,
       featureSearchResults: Object.entries({}),
       featureSearch: ""
     });
+  };
+
+  removeFeature = (e, feature, index) => {
+    e.preventDefault();
+
+    const featuresSelected = this.state.featuresSelected;
+    const featuresToSelect = this.state.featuresToSelect;
+
+    featuresSelected.splice(index, 1);
+    featuresToSelect.push(feature);
+
+    this.setState({ featuresSelected, featuresToSelect });
   };
 
   generateProject = e => {
@@ -178,7 +204,6 @@ class App extends Component {
     })
       .then(response => response.blob())
       .then(blob => {
-        console.log("Entraaa");
         var url = window.URL.createObjectURL(blob);
         var a = document.createElement("a");
         a.href = url;
@@ -190,23 +215,15 @@ class App extends Component {
       });
   };
 
-  handleNameChange = event => {
-    this.setState({ name: event.target.value });
-  };
-
-  handleLangChange = event => {
-    this.setState({ lang: event.target.value });
-  };
-
-  handleBuildChange = event => {
-    this.setState({ build: event.target.value });
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
   };
 
   render() {
     return (
       <Fragment>
         <div className="container">
-          <img src={logo} width="55%" alt="logo" className="mn-logo" />
+          <img src={logo} width="50%" alt="logo" className="mn-logo" />
           <div className="mn-container">
             <form onSubmit={this.generateProject} autoComplete="off">
               <Row>
@@ -216,8 +233,9 @@ class App extends Component {
                     s={12}
                     className="mn-input"
                     label="Name"
+                    name="name"
                     value={this.state.name}
-                    onChange={this.handleNameChange}
+                    onChange={this.handleChange}
                   />
                 </Col>
                 <Col s={4}>
@@ -225,7 +243,8 @@ class App extends Component {
                     s={12}
                     label="Language"
                     value={this.state.lang}
-                    onChange={this.handleLangChange}
+                    name="lang"
+                    onChange={this.handleChange}
                   >
                     <option value="java">Java</option>
                     <option value="kotlin">Kotlin</option>
@@ -237,7 +256,8 @@ class App extends Component {
                     s={12}
                     label="Build"
                     value={this.state.build}
-                    onChange={this.handleBuildChange}
+                    name="build"
+                    onChange={this.handleChange}
                   >
                     <option value="gradle">Gradle</option>
                     <option value="maven">Maven</option>
@@ -246,7 +266,6 @@ class App extends Component {
               </Row>
               <Row>
                 <Col s={6}>
-                  <Row>
                     <TextInput
                       className="mn-input"
                       s={12}
@@ -255,6 +274,12 @@ class App extends Component {
                       value={this.state.featureSearch}
                       onChange={this.searchFeature}
                     />
+                    {this.state.featureSearch &&
+                    this.state.featureSearchResults.length === 0 ? (
+                      <Col s={12}>
+                        <p className="grey-text">No results.</p>
+                      </Col>
+                    ) : null}
                     {this.state.featureSearchResults.map((feature, i) => (
                       <Col s={12} key={i}>
                         <Card
@@ -266,16 +291,29 @@ class App extends Component {
                         </Card>
                       </Col>
                     ))}
-                  </Row>
                 </Col>
                 <Col s={6}>
-                  {this.state.featuresSelected.length > 0 ? (
-                    <b>Selected features</b>
+                  <b>Selected features</b>
+                  {this.state.featuresSelected.length === 0 ? (
+                      <p className="grey-text">No features selected.</p>
                   ) : null}
                   <Row>
-                    {this.state.featuresSelected.map((helpTerm, i) => (
-                      <Card className="white" title={helpTerm[0]} key={i}>
-                        <p className="grey-text">{helpTerm[1]}</p>
+                    {this.state.featuresSelected.map((feature, i) => (
+                      <Card className="white" title={feature[0]} key={i}>
+                        <Row>
+                          <Col s={11}>
+                            <p className="grey-text">{feature[1]}</p>
+                          </Col>
+                          <Col s={1}>
+                            <Button
+                              floating
+                              small
+                              icon="close"
+                              className="black"
+                              onClick={(e) => this.removeFeature(e, feature, i)}
+                            />
+                          </Col>
+                        </Row>
                       </Card>
                     ))}
                   </Row>
@@ -298,39 +336,53 @@ class App extends Component {
           </div>
         </div>
         <div className="mn-footer">
-            <Modal open={this.state.info} header="What's this?" trigger={<Button floating icon="info" className="black" onClick={() => this.setState({info: true})}></Button>}>
-              <p>
-                Micronaut Initializr is an application web that allows to create Micronaut projects by an interface instead of use the console CLI. You can
-                choose a name to the project, the language (Java, Kotlin, Groovy), the build tool (Maven, Gradle) and the features you need to develop your software.
-              </p>
-            </Modal>
-            <a
-              href="https://twitter.com/micronautfw"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="mn-footer-logos"
-            >
-              <img
-                src="https://image.flaticon.com/icons/png/512/23/23931.png"
-                alt="Twitter"
-                height="30px"
-                weight="30px"
+          <Modal
+            open={this.state.info}
+            header="What's this?"
+            trigger={
+              <Button
+                floating
+                icon="info"
+                className="black"
+                onClick={() => this.setState({ info: true })}
               />
-            </a>
-            <a
-              href="https://github.com/micronaut-projects"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="mn-footer-logos"
-            >
-              <img
-                src="https://image.flaticon.com/icons/svg/25/25231.svg"
-                alt="GitHub"
-                height="30px"
-                weight="30px"
-              />
-            </a>
-          </div>
+            }
+          >
+            <p>
+              Micronaut Initializr is an application web that allows to create
+              Micronaut projects by an interface instead of use the console CLI.
+              You can choose a name to the project, the language (Java, Kotlin,
+              Groovy), the build tool (Maven, Gradle) and the features you need
+              to develop your software.
+            </p>
+          </Modal>
+          <a
+            href="https://twitter.com/micronautfw"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mn-footer-logos"
+          >
+            <img
+              src="https://image.flaticon.com/icons/png/512/23/23931.png"
+              alt="Twitter"
+              height="30px"
+              weight="30px"
+            />
+          </a>
+          <a
+            href="https://github.com/micronaut-projects"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mn-footer-logos"
+          >
+            <img
+              src="https://image.flaticon.com/icons/svg/25/25231.svg"
+              alt="GitHub"
+              height="30px"
+              weight="30px"
+            />
+          </a>
+        </div>
       </Fragment>
     );
   }
